cddecc1d400dbb7b4f6abceef9327905673072e8
AlexTerry101 <alexterry48@gmail.com>
2019-11-15 09:06:26 +0000

    Changed README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="coordinate_finder",
-    version="1.0",
+    version="1.0.1",
     author="Alex Terry & Alistair Heath",
     author_email="alexterry48@gmail.com.com",
     description="Package to find equidistant Lat & Long coordinates",
