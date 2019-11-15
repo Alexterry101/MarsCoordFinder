dc08cb5f09d9971c16604f94d598b27c42f4f3ac
AlexTerry101 <alexterry48@gmail.com>
2019-11-15 08:48:21 +0000

    Edited Setup.py to include more details & requirements

--- setup.py
+++ setup.py
@@ -1,32 +1,31 @@
 from setuptools import setup
 
-setup(name='coordinate_finder',
-      version='0.4',
-      description='Equidistaqnt Coordinate Finder',
-      packages=['coordinate_finder'],
-      author='Alex Terry & Alistair Heath',
-      author_email='alexterry48@gmail.com',
-      zip_safe=False,
-      include_package_data=True)
-
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="coordinate_finder",
     version="0.0.1",
-    author="EAlex Terry & Alistair Heath",
+    author="Alex Terry & Alistair Heath",
     author_email="alexterry48@gmail.com.com",
-    description="Package to find Lat & Long coordinates",
+    description="Package to find equidistant Lat & Long coordinates",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/Alexterry101/MarsCoordFinder",
+    packages=['coordinte_finder'],
+    install_requires=[            # I get to this in a second
+          'scikit_learn',
+          'pandas',
+          'matplotlib',
+          'numpy',
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Data Scientists',
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-)
+    zip_safe=False,
+    include_package_data=True)
